162addaef7909a728bb85775679dd394ba48bcf1
Henry Dixon <hdixon@cmu.edu>
2017-04-18 02:56:39 -0400

    slimmed down php

--- construction/submit.php
+++ construction/submit.php
@@ -2,25 +2,7 @@
 
 <body>
 
-Your email address is: <?php echo $_POST["email"]; ?>
-
-<?php
-/*include 'index.html';
-
-if(isset($_POST['email'])) {
-    $data = $_POST['email'] . "\n";
-    $ret = file_put_contents('emails.txt', $data, FILE_APPEND | LOCK_EX);
-    if($ret === false) {
-        die('There was an error writing this file');
-    }
-    else {
-        echo "$ret bytes written to file";
-    }
-}
-else {
-   die('no post data to process');
-}
-*/?>
+Thank you for signing up. We'll contact you soon enough!
 
 <?php
 $myfile = fopen("emails.txt", "w") or die("Unable to open file!");
