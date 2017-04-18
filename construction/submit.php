dad0dde0cd6741ce3204d1a128d769988a5e89a3
Henry Dixon <hdixon@cmu.edu>
2017-04-18 02:55:32 -0400

    FINALLY got php to work wow

--- construction/submit.php
+++ construction/submit.php
@@ -5,10 +5,27 @@
 Your email address is: <?php echo $_POST["email"]; ?>
 
 <?php
+/*include 'index.html';
 
+if(isset($_POST['email'])) {
+    $data = $_POST['email'] . "\n";
+    $ret = file_put_contents('emails.txt', $data, FILE_APPEND | LOCK_EX);
+    if($ret === false) {
+        die('There was an error writing this file');
+    }
+    else {
+        echo "$ret bytes written to file";
+    }
+}
+else {
+   die('no post data to process');
+}
+*/?>
+
+<?php
 $myfile = fopen("emails.txt", "w") or die("Unable to open file!");
-$email = $_POST["email"];
-fwrite($myfile, $email);
+$txt = $_POST['email'] . "\n";
+fwrite($myfile, $txt);
 fclose($myfile);
 ?>
 </body>
