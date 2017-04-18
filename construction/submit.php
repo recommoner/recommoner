763d37e5975b2ffd4a46f6c36dec960ff4bd7dec
Henry Dixon <hdixon@cmu.edu>
2017-04-18 06:57:24 -0400

    we can now collect emails

--- construction/submit.php
+++ construction/submit.php
@@ -5,7 +5,7 @@
 Thank you for signing up. We'll contact you soon enough!
 
 <?php
-$myfile = fopen("emails.txt", "w") or die("Unable to open file!");
+$myfile = fopen("emails.txt", "a") or die("Unable to open file!");
 $txt = $_POST['email'] . "\n";
 fwrite($myfile, $txt);
 fclose($myfile);
